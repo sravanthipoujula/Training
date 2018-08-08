0a5c2bafe6753db390e217d37f62011543ab7546
sravanthi <sravanthi.poujula@macys.com>
2018-08-08 14:17:56 -0700

    updated

--- src/LnkdList/MergePoint.java
+++ src/LnkdList/MergePoint.java
@@ -9,6 +9,8 @@ package LnkdList;
 //               6
 
 
+import java.util.HashSet;
+
 public class MergePoint {
     static class Node{
         int data;
@@ -38,22 +40,77 @@ public class MergePoint {
         }
     }
 
-    public void findMergePoint(){
+    public void findMergePointWithFlag(){
         if(head1 == null || head2 == null){
             return;
         }
-
         Node temp1 = head1;
-        Node temp2 = head2;
         while(temp1.next != null){
-            if(temp1.visited || temp2.visited){
-                System.out.println("Found merge point @ " + temp1.data + "or" + temp2.data);
+            temp1.visited = true;
+            temp1 = temp1.next;
+        }
+        Node temp2 = head2;
+        while(temp2.next != null){
+            if(temp2.visited){
+                System.out.println("Found merge point @ " + temp2.data);
                 return;
             }
-            temp1.visited = true;
-            temp2.visited = true;
-            temp1 = temp1.next;
             temp2 = temp2.next;
+        }
+    }
+
+    public void findMergePointWithSet(){
+        if(head1 == null || head2 == null){
+            return;
+        }
+        Node temp1 = head1;
+        HashSet<Node> set = new HashSet<>();
+        while(temp1.next != null){
+            set.add(temp1);
+            temp1 = temp1.next;
+        }
+
+        Node temp2 = head2;
+        while(temp2.next != null){
+            if(set.contains(temp2)){
+                System.out.println("Found merge point @ " + temp2.data);
+                return;
+            }
+            temp2 =  temp2.next;
+        }
+    }
+    public int getCount(Node node){
+        Node temp = node;
+        int count = 0;
+        while (temp != null){
+            count ++;
+            temp = temp.next;
+        }
+        return count;
+    }
+    public void findMergePoint(int count, Node node1, Node node2){
+        for(int i=0; i< count; i++){
+            node1 = node1.next;
+        }
+        while (node1 != null && node2 != null){
+            if(node1.data == node2.data){
+                System.out.println("Found merge point @ "+ node1.data);
+                return;
+            }
+            node1 = node1.next;
+            node2 = node2.next;
+        }
+    }
+    public void findMergePointWithMath(){
+        if(head1 == null || head2 == null){
+            return;
+        }
+        int count1 = getCount(head1);
+        int count2 = getCount(head2);
+        if(count1 > count2){
+            findMergePoint(count1 - count2,head1, head2);
+        }else{
+            findMergePoint(count2 - count1,head2,head1);
         }
 
     }
@@ -70,6 +127,8 @@ public class MergePoint {
         list.head2.next = list.head1.next.next.next;
 
         list.printLists();
-        list.findMergePoint();
+        list.findMergePointWithFlag();
+        list.findMergePointWithSet();
+        list.findMergePointWithMath();
     }
 }
